1988f8c75ef07ef78185f0df5c08dcc97ebb42ba
dogukanzengin <dogukanzen@gmail.com>
2019-11-01 14:07:34 +0300

    Try with resources code block added for closing of reader resource

--- src/main/java/com/zopa/quote/helper/LenderMapper.java
+++ src/main/java/com/zopa/quote/helper/LenderMapper.java
@@ -3,8 +3,8 @@ package com.zopa.quote.helper;
 import com.zopa.quote.model.Lender;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,18 +12,21 @@ import java.util.List;
 public class LenderMapper {
 
     private static String[] HEADERS = {"Lender","Rate","Available"};
-    public static List<Lender> map(String filePath) throws IOException {
+    public static List<Lender> map(String filePath){
 
         List<Lender> lenderList = new ArrayList<>();
-        Reader in = new FileReader(filePath);
+        Iterable<CSVRecord> csvRecords;
+        try(Reader in = new FileReader(filePath)){
+            csvRecords = CSVFormat.DEFAULT.withHeader(HEADERS).withFirstRecordAsHeader().parse(in);
+            for(CSVRecord record : csvRecords){
+                Lender lender = new Lender();
+                lender.setName(record.get("Lender"));
+                lender.setLendAmount(Integer.valueOf(record.get("Available")));
+                lender.setRate(Double.valueOf(record.get("Rate")));
+                lenderList.add(lender);
+            }
+        }catch (Exception e){
 
-        Iterable<CSVRecord> csvRecords = CSVFormat.DEFAULT.withHeader(HEADERS).withFirstRecordAsHeader().parse(in);
-        for(CSVRecord record : csvRecords){
-            Lender lender = new Lender();
-            lender.setName(record.get("Lender"));
-            lender.setLendAmount(Integer.valueOf(record.get("Available")));
-            lender.setRate(Double.valueOf(record.get("Rate")));
-            lenderList.add(lender);
         }
         return lenderList;
     }
